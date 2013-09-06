a60e0413e28a32121e23c091112ff77eed2dcc47
Edmund Sumbar <esumbar@gmail.com>
2013-09-05 20:06:33 -0600

    Exercises 5 and 6 completed

--- lib_tester_db.erl
+++ lib_tester_db.erl
@@ -1,6 +1,6 @@
 -module (lib_tester_db).
 
--export ([init_tables/1, delete_tables/0]).
+-export ([init_tables/0, delete_tables/0]).
 -export ([init_tester/1, least_loaded/0]).
 -export ([increment_load/2, decrement_load/2]).
 -export ([get_requests/1]).
@@ -10,56 +10,43 @@
     load,           % int()
     requests=[]}).  % [int()]
 
-init_tables(FileName) ->
-    dets:open_file(dets_tester_db,
-        [{file, FileName}, {keypos, #state.srv_name}]),
-    case dets:info(dets_tester_db, size) of
-        0 ->
-            ets:new(ets_tester_db, [named_table, {keypos, #state.srv_name}]);
-        Size when is_integer(Size), Size > 0 ->
-            ets:new(ets_tester_db, [named_table, {keypos, #state.srv_name}]),
-            dets:to_ets(dets_tester_db, ets_tester_db)
-    end.
+init_tables() ->
+    ok = mnesia:wait_for_tables([state], 60000).
 
 delete_tables() ->
-    ets:delete(ets_tester_db),
-    dets:close(dets_tester_db).
+    mnesia:clear_table(state).
 
 init_tester(Name) ->
-    case ets:member(ets_tester_db, Name) of
-        false ->
-            ets:insert(ets_tester_db, #state{srv_name=Name, load=0}),
-            dets:insert(dets_tester_db, #state{srv_name=Name, load=0});
-        true ->
-            true
+    case mnesia:dirty_read(state, Name) of
+        [] ->
+            mnesia:dirty_write(#state{srv_name=Name, load=0});
+        [_] ->
+            ok
     end.
 
 least_loaded() ->
-    [Acc0] = ets:lookup(ets_tester_db, ets:first(ets_tester_db)),
-    #state{srv_name=Name} = ets:foldl(
-        fun(#state{load=LoadElem} = Elem, #state{load=LoadAccIn}) when LoadElem < LoadAccIn ->
-                Elem;
-            (_, AccIn) ->
-                AccIn
-        end,
-        Acc0,
-        ets_tester_db),
-    Name.
+    Name = mnesia:dirty_first(state),
+    least_loaded(mnesia:dirty_next(state, Name), Name).
+
+least_loaded('$end_of_table', Acc) ->
+    Acc;
+least_loaded(Next, Acc) ->
+    Next1 = mnesia:dirty_next(state, Next),
+    [#state{load=AccLoad}] = mnesia:dirty_read(state, Acc),
+    [#state{load=NextLoad}] = mnesia:dirty_read(state, Next),
+    if
+        AccLoad > NextLoad -> least_loaded(Next1, Next);
+        AccLoad =< NextLoad -> least_loaded(Next1, Acc)
+    end.
 
 increment_load(Name, K) ->
-    [#state{load=Load, requests=Requests}] = ets:lookup(ets_tester_db, Name),
-    ets:update_element(ets_tester_db, Name,
-        [{#state.load, Load+1}, {#state.requests, [K|Requests]}]),
-    dets:insert(dets_tester_db,
-        #state{srv_name=Name, load=Load+1, requests=[K|Requests]}).
+    [#state{load=Load, requests=Requests}] = mnesia:dirty_read(state, Name),
+    mnesia:dirty_write(#state{srv_name=Name, load=Load+1, requests=[K|Requests]}).
 
 decrement_load(Name, K) ->
-    [#state{load=Load, requests=Requests}] = ets:lookup(ets_tester_db, Name),
-    ets:update_element(ets_tester_db, Name,
-        [{#state.load, Load-1}, {#state.requests, lists:delete(K, Requests)}]),
-    dets:insert(dets_tester_db,
-        #state{srv_name=Name, load=Load-1, requests=lists:delete(K, Requests)}).
+    [#state{load=Load, requests=Requests}] = mnesia:dirty_read(state, Name),
+    mnesia:dirty_write(#state{srv_name=Name, load=Load-1, requests=lists:delete(K, Requests)}).
 
 get_requests(Name) ->
-    [#state{requests=Requests}] = ets:lookup(ets_tester_db, Name),
+    [#state{requests=Requests}] = mnesia:dirty_read(state, Name),
     Requests.
